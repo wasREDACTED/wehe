1c8a79feaddccaa5e5510f52671076d3e00f9f1c
wasREDACTED <greyson.yant@gmail.com>
2018-06-16 21:58:15 -0700

    dart web quickstart

---
+++ quickstart/web/main.dart
@@ -0,0 +1,13 @@
+import 'package:angular/angular.dart';
+import 'package:angular_app/app_component.template.dart' as ng;
+
+var container = new Container (
+  child: new Center(
+    child: new Text(
+    "Welcome"
+    )
+  )
+  width: 320.0,
+  height:240.0
+  color: Colors.grey[300],
+)
